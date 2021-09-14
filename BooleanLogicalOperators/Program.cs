3b62444180dc82f08f26c6e59cd8e4cf60d69b41
KalerKusik <karel.kuusik1@gmail.com>
2021-09-14 12:01:13 +0300

    yeeees

---
+++ BooleanLogicalOperators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BooleanLogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus
+            //programm küsib kasutajal sisestada parool
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus kui ka parool on õiged
+            //kui mõlemad, kasutajatunnus ja parool on õiged, programm kuvab konsoolis "Tere tulemast!"
+            //kui kasutajatunnus või parool on vale, siis programm kuvab "sisselogimine ebaõnnestus. Proovi uuesti"
+            //kt: admin, pr: admin1234
+
+            Console.WriteLine("Username:");
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            string PassWord = Console.ReadLine();
+
+            if (UserName == "admin" && PassWord == "admin1234")
+            {
+                Console.WriteLine("Tere Tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Sisselogimine Ebaõnnestus. Proovi Uuesti");
+            }
+
+    }
+}
+
+        }
---
+++ Login/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus
+            //programm küsib kasutajal sisestada parool
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus kui ka parool on õiged
+            //kui mõlemad, kasutajatunnus ja parool on õiged, programm kuvab konsoolis "Tere tulemast!"
+            //kui kasutajatunnus või parool on vale, siis programm kuvab "sisselogimine ebaõnnestus. Proovi uuesti"
+            //kt: admin, pr: admin1234
+
+            Console.WriteLine("Username:");
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            string PassWord = Console.ReadLine();
+
+            if (UserName != "admin" || PassWord != "admin1234")
+            {
+                Console.WriteLine("Sisselogimine ebaõnnestus. Proovi uuesti.");
+            }
+            else
+            {
+                Console.WriteLine("Tere Tulemast!");
+            }
+        }
+    }
+}
